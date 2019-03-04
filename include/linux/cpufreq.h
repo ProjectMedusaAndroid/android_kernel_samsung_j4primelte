2b77b67ba097409c2ea2d1f1acb9ef8995e87b53
emabertax <47644322+emabertax@users.noreply.github.com>
2019-03-04 22:57:46 +0100

    added interactivex governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -559,6 +559,9 @@ extern struct cpufreq_governor cpufreq_gov_smartmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
 extern struct cpufreq_governor cpufreq_gov_bioshock;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_bioshock)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
+extern struct cpufreq_governor cpufreq_gov_interactivex;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
 #endif
 
 /*********************************************************************
