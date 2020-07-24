83a3eb9921bf8d4fdf9418116f1c80a4fec2cc9a
Batu33-TR <batuhanarslanoglu4@gmail.com>
2020-07-24 17:22:25 +0300

    Revert "zzmoove: Added zzmoove governor by @zanezam"
    This reverts commit e48c09dee7dcceed4e9a4e07705aefb149812e29.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -544,9 +544,6 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intelliactive)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
-extern struct cpufreq_governor cpufreq_gov_zzmoove;
-#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELECTRON)
 extern struct cpufreq_governor cpufreq_gov_electron;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_electron)
