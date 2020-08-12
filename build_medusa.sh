b407597a76a9e5fd44cf45fbea72dcb50042fec3
Batu33-TR <batuhanarslanoglu4@gmail.com>
2020-08-12 21:42:26 +0300

    build_medusa.sh: Use j4primelte_kali_defconfig

--- build_medusa.sh
+++ build_medusa.sh
@@ -16,7 +16,7 @@ export SUBARCH=arm
 
 # Set kernel name and defconfig
 export VERSION=MedusaKernel-v1.7-NeMutluTürkümDiyene
-export DEFCONFIG=sec8917_defconfig
+export DEFCONFIG=j4primelte_kali_defconfig
 
 # Keep it as is
 export LOCALVERSION=-$VERSION
